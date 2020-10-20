895b12628ca420bedbf7283eff476e541f3d56d3
James OLeary <olearyj234@gmail.com>
2020-10-19 23:42:09 -0400

    Created Entities

---
+++ app/src/main/java/com/AYC/canalguide/data/entities/BridgeGateMarker.kt
@@ -0,0 +1,35 @@
+package com.AYC.canalguide.data.entities
+
+import androidx.room.Entity
+import androidx.room.Ignore
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "BridgeGateMarker")
+data class BridgeGateMarker (
+    @Ignore
+    override val lat: Double,
+    @Ignore
+    override val lng: Double,
+    @Ignore
+    override val name: String,
+    @Ignore
+    override val bodyOfWater: String,
+    @Ignore
+    override val mile: Double,
+
+    val location: String?,
+    val phone: String?,
+    val clearanceClosed: Double?,
+    val clearanceOpened: Double?,
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0
+): MapMarker(lat, lng, name, bodyOfWater, mile) {
+
+    companion object {
+        val sampleData = listOf(
+            BridgeGateMarker(43.18955, -73.58102, "Guard Gate Crockers Reef", "Ft Miller", 31.3, "CHamplain", "518-555-5555", 3.2, 2.3),
+            BridgeGateMarker(42.80340, -73.70890, "Guard Gate fake data", "Ft Miller!!", 31.2, "CHamplain", "518-555-5555", 3.2, 2.3)
+        )
+    }
+
+}
---
+++ app/src/main/java/com/AYC/canalguide/data/entities/MapMarker.kt
@@ -0,0 +1,11 @@
+package com.AYC.canalguide.data.entities
+
+import androidx.room.PrimaryKey
+
+abstract class MapMarker (
+    open val lat: Double,
+    open val lng: Double,
+    open val name: String,
+    open val bodyOfWater: String,
+    open val mile: Double
+)
