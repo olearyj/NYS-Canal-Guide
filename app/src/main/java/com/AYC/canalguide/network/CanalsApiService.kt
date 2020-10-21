2a690a6d665d506a410e5382483954c47b572e2e
James OLeary <olearyj234@gmail.com>
2020-10-20 22:38:15 -0400

    created api service class, connectivityInceptor and Result classes added to help

---
+++ app/src/main/java/com/AYC/canalguide/network/CanalsApiService.kt
@@ -0,0 +1,58 @@
+package com.AYC.canalguide.network
+
+import com.AYC.canalguide.data.entities.BridgeGateMarker
+import com.AYC.canalguide.data.xml_classes.LiftBridges
+import com.tickaroo.tikxml.TikXml
+import com.tickaroo.tikxml.retrofit.TikXmlConverterFactory
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.ResponseBody
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+
+interface CanalsApiService {
+
+
+    @GET("liftbridges.xml")
+    suspend fun getLiftBridges(): Response<LiftBridges>
+
+
+    @GET("liftbridges.xml")
+    suspend fun getLiftBridgesText(): ResponseBody
+
+
+    companion object {
+
+        operator fun invoke(connectivityInterceptor: ConnectivityInterceptor): CanalsApiService {
+            val loggingInterceptor = HttpLoggingInterceptor().apply {
+                this.level = HttpLoggingInterceptor.Level.BODY
+            }
+
+            val okHttpClient = OkHttpClient.Builder()
+                .addInterceptor(connectivityInterceptor)
+                .addInterceptor(loggingInterceptor)
+                .build()
+
+            val retrofit = Retrofit.Builder()
+                .client(okHttpClient)
+                .baseUrl("http://www.canals.ny.gov/xml/")
+                //.addConverterFactory(GsonConverterFactory.create())
+                .addConverterFactory(
+                    TikXmlConverterFactory.create(
+                        TikXml.Builder()
+                            .exceptionOnUnreadXml(false) // CRTL + F on this site for more info: https://github.com/Tickaroo/tikxml/blob/master/docs/AnnotatingModelClasses.md
+                            //.writeDefaultXmlDeclaration(false)
+                            .build()
+                    )
+                )
+                .build()
+
+            return retrofit.create(CanalsApiService::class.java)
+        }
+    }
+
+}
---
+++ app/src/main/java/com/AYC/canalguide/repos/Result.kt
@@ -0,0 +1,6 @@
+package com.AYC.canalguide.repos
+
+sealed class Result<out T: Any> {
+    data class Success<out T : Any>(val data: T) : Result<T>()
+    data class Error(val exception: Exception) : Result<Nothing>()
+}
