66e6a98a799dfdee62902ad8a471ef4dfc034848
James OLeary <olearyj234@gmail.com>
2020-10-21 23:55:59 -0400

    Added more APIs to ApiService and created constant file

--- app/src/main/java/com/AYC/canalguide/network/CanalsApiService.kt
+++ app/src/main/java/com/AYC/canalguide/network/CanalsApiService.kt
@@ -1,30 +1,103 @@
 package com.AYC.canalguide.network
 
-import com.AYC.canalguide.data.entities.BridgeGateMarker
+import com.AYC.canalguide.Constants.BASE_URL
 import com.AYC.canalguide.data.xml_classes.GuardGates
 import com.AYC.canalguide.data.xml_classes.LiftBridges
 import com.tickaroo.tikxml.TikXml
 import com.tickaroo.tikxml.retrofit.TikXmlConverterFactory
-import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.ResponseBody
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Response
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 
 interface CanalsApiService {
 
+    // Lift bridges and guard gates
     @GET("liftbridges.xml")
     suspend fun getLiftBridgesText(): ResponseBody
-
     @GET("liftbridges.xml")
     suspend fun getLiftBridges(): Response<LiftBridges>
-
     @GET("guardgates.xml")
     suspend fun getGuardGates(): Response<GuardGates>
+
+    // Locks
+    @GET("locks.xml")
+    suspend fun getLocks(): Response<Any>
+
+
+    // Rentals and cruises
+    @GET("boatsforhire.xml")
+    suspend fun getRentalsCruises(): Response<Any>
+
+    // Boat launches
+    @GET("canalwatertrail.xml")
+    suspend fun getBoatLaunches(): Response<Any>
+
+    // Marinas
+    @GET("marinas.xml")
+    suspend fun getMarinas(): Response<Any>
+
+
+    /**
+     * Extra API's to consider using below
+     */
+
+    // Lodging
+    @GET("lodging.xml")
+    suspend fun getLodging(): Response<Any>
+
+    // Heritage sites
+    @GET("heritagesites.xml")
+    suspend fun getHeritageSites(): Response<Any>
+
+    // Calendar events
+    @GET("calendar.xml")
+    suspend fun getCalendarEvents(): Response<Any>
+
+
+    /**
+     * Navigation
+     */
+
+    @GET("navinfo-{region}.xml")
+    suspend fun getNavInfo(
+        @Path("region") region: String
+    ): Response<GuardGates>
+
+
+//    @GET("navinfo-hudsonriver.xml")
+//    suspend fun getGuardGates(): Response<GuardGates>
+//    @GET("navinfo-champlain.xml")
+//    suspend fun getGuardGates(): Response<GuardGates>
+//    @GET("navinfo-fortedward.xml")
+//    suspend fun getGuardGates(): Response<GuardGates>
+//    @GET("navinfo-erieeastern.xml")
+//    suspend fun getGuardGates(): Response<GuardGates>
+//    @GET("navinfo-frankfortharbor.xml")
+//    suspend fun getGuardGates(): Response<GuardGates>
+//    @GET("navinfo-uticaharbor.xml")
+//    suspend fun getGuardGates(): Response<GuardGates>
+//    @GET("navinfo-oswego.xml")
+//    suspend fun getGuardGates(): Response<GuardGates>
+//    @GET("navinfo-eriecentral.xml")
+//    suspend fun getGuardGates(): Response<GuardGates>
+//    @GET("navinfo-onondagalake.xml")
+//    suspend fun getGuardGates(): Response<GuardGates>
+//    @GET("navinfo-cayugaseneca.xml")
+//    suspend fun getGuardGates(): Response<GuardGates>
+//    @GET("navinfo-cayugalake.xml")
+//    suspend fun getGuardGates(): Response<GuardGates>
+//    @GET("navinfo-senecalake.xml")
+//    suspend fun getGuardGates(): Response<GuardGates>
+//    @GET("navinfo-eriewestern.xml")
+//    suspend fun getGuardGates(): Response<GuardGates>
+//    @GET("navinfo-geneseeriver.xml")
+//    suspend fun getGuardGates(): Response<GuardGates>
+//    @GET("navinfo-ellicottcreek.xml")
+//    suspend fun getGuardGates(): Response<GuardGates>
 
 
     companion object {
@@ -41,7 +114,7 @@ interface CanalsApiService {
 
             val retrofit = Retrofit.Builder()
                 .client(okHttpClient)
-                .baseUrl("http://www.canals.ny.gov/xml/")
+                .baseUrl(BASE_URL)
                 //.addConverterFactory(GsonConverterFactory.create())
                 .addConverterFactory(
                     TikXmlConverterFactory.create(
