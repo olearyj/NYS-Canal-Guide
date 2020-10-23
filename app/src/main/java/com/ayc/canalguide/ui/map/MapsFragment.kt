641afd02039fb949e2957d6edf3cdb419663253c
James OLeary <olearyj234@gmail.com>
2020-10-23 16:31:59 -0400

    Added the rest of the morker types to the map and observed the VM

--- app/src/main/java/com/ayc/canalguide/ui/map/MapsFragment.kt
+++ app/src/main/java/com/ayc/canalguide/ui/map/MapsFragment.kt
@@ -1,50 +1,51 @@
 package com.ayc.canalguide.ui.map
 
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import androidx.fragment.app.Fragment
-
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
+import com.ayc.canalguide.MainActivity
 import com.ayc.canalguide.R
-import com.ayc.canalguide.data.entities.LockMarker
 import com.ayc.canalguide.data.entities.MapMarker
-
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
-import com.google.android.gms.maps.model.MarkerOptions
 
+/**
+ * Be sure to know the difference between the two types with the word "marker" in it:
+ * MapMarker(com.ayc.canalguide.data.entities.MapMarker) - data class to store data from the nys canal corp, stored in viewModel
+ * Marker(com.google.android.gms.maps.model.Marker) - Reference to the icon that's placed on the google map view, stored in this class
+ */
 class MapsFragment : Fragment(R.layout.fragment_maps) {
 
 
     private val mapsViewModel: MapsViewModel by activityViewModels()
 
+    // List of references to the markers on the map by category
     private val lockMarkers = mutableListOf<Marker>()
+    private val bridgeGateMarkers = mutableListOf<Marker>()
+    private val marinaMarkers = mutableListOf<Marker>()
+    private val cruiseMarkers = mutableListOf<Marker>()
+    private val launchMarkers = mutableListOf<Marker>()
+    private val navInfoMarkers = mutableListOf<Marker>()
 
     private lateinit var map: GoogleMap
 
+
+    /**
+     * When the map is ready, move to start camera location, observe filter states & data from view model, set custom info window
+     */
     @SuppressLint("MissingPermission")
     private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        //val sydney = LatLng(-34.0, 151.0)
-        //googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        //googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        map = googleMap
 
         // Move camera to starting position
         val saratoga = LatLng(43.0616419,-73.7719178)
@@ -53,31 +54,6 @@ class MapsFragment : Fragment(R.layout.fragment_maps) {
         if (MainActivity.hasPermissions(requireContext(), MainActivity.LOCATION_PERMISSION))
             map.isMyLocationEnabled = true
 
-        fun createMarkerObserver(markerList: MutableList<Marker>): Observer<List<MapMarker>> =
-                Observer<List<MapMarker>> { markers ->
-                    // Refresh the marker list with the updated list
-                    markerList.removeMarkersFromMapAndClearList()
-                    for (marker in markers)
-                        markerList += googleMap.addMarker( marker.getMarkerOptions() )
-                }
-
-        fun createFilterStateObserver(markerList: MutableList<Marker>, mapMarkers: List<MapMarker>?): Observer<Boolean> =
-                Observer { isChecked ->
-                    if (isChecked)  // Add markers to map
-                        for (marker in mapMarkers ?: return@Observer)
-                            markerList += googleMap.addMarker( marker.getMarkerOptions() )
-                    else
-                        markerList.removeMarkersFromMapAndClearList()
-                }
-
-        mapsViewModel.bridgeGateMarkers.observe(viewLifecycleOwner) { markers ->
-            for (marker in markers)
-                googleMap.addMarker( marker.getMarkerOptions() )
-        }
-
-        mapsViewModel.lockMarkers.observe(viewLifecycleOwner, createMarkerObserver(lockMarkers))
-        mapsViewModel.lockFilterState.observe(viewLifecycleOwner, createFilterStateObserver(lockMarkers, mapsViewModel.lockMarkers.value))
-
         // Set custom info window so an icon appears on the right - TODO
         //googleMap.setInfoWindowAdapter(CanalInfoWindowAdapter())
 
@@ -85,10 +61,45 @@ class MapsFragment : Fragment(R.layout.fragment_maps) {
         googleMap.setOnInfoWindowClickListener {
             Toast.makeText(context, "CLICKED INFO WINDOW", Toast.LENGTH_SHORT).show()
         }
+
+        fun createMarkerObserver(markerList: MutableList<Marker>, filterStateIsChecked: LiveData<Boolean>): Observer<List<MapMarker>> =
+                Observer<List<MapMarker>> { markers ->
+                    if (filterStateIsChecked.value != true) return@Observer
+
+                    // Refresh the marker list with the updated list
+                    markerList.removeMarkersFromMapAndClearList()
+                    for (marker in markers)
+                        markerList += googleMap.addMarker( marker.getMarkerOptions() )
+                }
+
+        fun <T: MapMarker> createFilterStateObserver(markerList: MutableList<Marker>, mapMarkers: LiveData<List<T>>): Observer<Boolean> =
+                Observer { isChecked ->
+                    if (isChecked)  // Add markers to map
+                        for (marker in mapMarkers.value ?: return@Observer)
+                            markerList += googleMap.addMarker( marker.getMarkerOptions() )
+                    else
+                        markerList.removeMarkersFromMapAndClearList()
+                }
+
+        // Observe MapMarkers in viewModel
+        mapsViewModel.lockMarkers.observe(viewLifecycleOwner, createMarkerObserver(lockMarkers, mapsViewModel.lockFilterState))
+        mapsViewModel.bridgeGateMarkers.observe(viewLifecycleOwner, createMarkerObserver(bridgeGateMarkers, mapsViewModel.bridgeGateFilterState))
+        mapsViewModel.marinaMarkers.observe(viewLifecycleOwner, createMarkerObserver(marinaMarkers, mapsViewModel.marinaFilterState))
+        mapsViewModel.cruiseMarkers.observe(viewLifecycleOwner, createMarkerObserver(cruiseMarkers, mapsViewModel.cruiseFilterState))
+        mapsViewModel.launchMarkers.observe(viewLifecycleOwner, createMarkerObserver(launchMarkers, mapsViewModel.launchFilterState))
+        mapsViewModel.navInfoMarkers.observe(viewLifecycleOwner, createMarkerObserver(navInfoMarkers, mapsViewModel.navInfoFilterState))
+
+        // Observe filter states
+        mapsViewModel.lockFilterState.observe(viewLifecycleOwner, createFilterStateObserver(lockMarkers, mapsViewModel.lockMarkers))
+        mapsViewModel.bridgeGateFilterState.observe(viewLifecycleOwner, createFilterStateObserver(bridgeGateMarkers, mapsViewModel.bridgeGateMarkers))
+        mapsViewModel.marinaFilterState.observe(viewLifecycleOwner, createFilterStateObserver(marinaMarkers, mapsViewModel.marinaMarkers))
+        mapsViewModel.cruiseFilterState.observe(viewLifecycleOwner, createFilterStateObserver(cruiseMarkers, mapsViewModel.cruiseMarkers))
+        mapsViewModel.launchFilterState.observe(viewLifecycleOwner, createFilterStateObserver(launchMarkers, mapsViewModel.launchMarkers))
+        mapsViewModel.navInfoFilterState.observe(viewLifecycleOwner, createFilterStateObserver(navInfoMarkers, mapsViewModel.navInfoMarkers))
     }
 
     private fun MutableList<Marker>.removeMarkersFromMapAndClearList() {
-        for (marker in lockMarkers)
+        for (marker in this)
             marker.remove()
         this.clear()
     }
