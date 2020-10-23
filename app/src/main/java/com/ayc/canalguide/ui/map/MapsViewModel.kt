7cd2bc442782cb2f0b291731288fe892e5869f4b
James OLeary <olearyj234@gmail.com>
2020-10-23 16:27:54 -0400

    exposed markers in the viewmodel and addrd filter states

--- app/src/main/java/com/ayc/canalguide/ui/map/MapsViewModel.kt
+++ app/src/main/java/com/ayc/canalguide/ui/map/MapsViewModel.kt
@@ -3,6 +3,7 @@ package com.ayc.canalguide.ui.map
 import androidx.lifecycle.ViewModel
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.liveData
 import com.ayc.canalguide.repos.MarkerRepository
 
@@ -10,12 +11,24 @@ class MapsViewModel @ViewModelInject constructor(
         markerRepository: MarkerRepository
 ): ViewModel() {
 
+    // Get markers from repo
     //val bridgeGateMarkers = markerRepository.loadSampleBridgeGateMarkers()
     val bridgeGateMarkers = markerRepository.loadBridgeGateMarkers()
-
     val lockMarkers = markerRepository.loadLockMarkers()
+    val marinaMarkers = markerRepository.loadMarinaMarkers()
+    val launchMarkers = markerRepository.loadLaunchMarkers()
+    val cruiseMarkers = markerRepository.loadCruiseMarkers()
+    val navInfoMarkers = Transformations.map(markerRepository.loadNavInfoMarkers()) { markers ->
+        // Remove any markers that don't fall into a category
+        markers.filter { marker -> marker.getBitmapDescriptor() != null }
+    }
 
-
+    // Filter states
     val lockFilterState = MutableLiveData(true)
+    val bridgeGateFilterState = MutableLiveData(true)
+    val marinaFilterState = MutableLiveData(true)
+    val launchFilterState = MutableLiveData(true)
+    val cruiseFilterState = MutableLiveData(true)
+    val navInfoFilterState = MutableLiveData(false)
 
 }
