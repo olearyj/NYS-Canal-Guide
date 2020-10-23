9952d80d402ac81096f9741f88ce46bcce9a7fb2
James OLeary <olearyj234@gmail.com>
2020-10-23 16:26:22 -0400

    Added latlon converter for navinto xml parsing

---
+++ app/src/main/java/com/ayc/canalguide/utils/LatLongConverter.kt
@@ -0,0 +1,18 @@
+package com.ayc.canalguide.utils
+
+import com.tickaroo.tikxml.TypeConverter
+
+/**
+ * Some lat / long data from the endpoint has trailing whitespace so a custom converter from string to double is a must
+ */
+class LatLongConverter: TypeConverter<Double> {
+
+    override fun read(value: String?): Double {
+        return value!!.replace("[^0-9.\\-]".toRegex(),"").toDouble()
+    }
+
+    override fun write(value: Double?): String {
+        return value.toString()
+    }
+
+}
