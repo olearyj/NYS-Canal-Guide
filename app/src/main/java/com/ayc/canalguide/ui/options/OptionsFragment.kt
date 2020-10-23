f8d32c01c3d07892d70748717988fac24d9f657e
James OLeary <olearyj234@gmail.com>
2020-10-23 16:50:09 -0400

    made custom filter xml layouts with data binding

--- app/src/main/java/com/ayc/canalguide/ui/options/OptionsFragment.kt
+++ app/src/main/java/com/ayc/canalguide/ui/options/OptionsFragment.kt
@@ -4,21 +4,19 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.ayc.canalguide.R
-import com.ayc.canalguide.databinding.FragmentOptions2Binding
 import com.ayc.canalguide.databinding.FragmentOptionsBinding
 import com.ayc.canalguide.ui.map.MapsViewModel
 import com.ayc.canalguide.utils.viewBinding
-import kotlinx.android.synthetic.main.fragment_options.*
 
 /**
  * A placeholder fragment containing a simple view.
  */
-class OptionsFragment : Fragment(R.layout.fragment_options2) {
+class OptionsFragment : Fragment(R.layout.fragment_options) {
 
 
     private val mapsViewModel: MapsViewModel by activityViewModels()
 
-    private val binding by viewBinding(FragmentOptions2Binding::bind)
+    private val binding by viewBinding(FragmentOptionsBinding::bind)
 
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
