821d0c5ca989bbea290decc8ef0db53391c97a84
James OLeary <olearyj234@gmail.com>
2020-11-03 17:43:22 -0500

    Cleaned mapfragment and activity_main

--- app/src/main/java/com/ayc/canalguide/ui/map/MapsFragment.kt
+++ app/src/main/java/com/ayc/canalguide/ui/map/MapsFragment.kt
@@ -24,7 +24,7 @@ import com.google.android.gms.maps.model.Marker
 /**
  * Be sure to know the difference between the two types with the word "marker" in it:
  * MapMarker(com.ayc.canalguide.data.entities.MapMarker) - data class to store data from the nys canal corp, stored in viewModel
- * Marker(com.google.android.gms.maps.model.Marker) - Reference to the icon that's placed on the google map view, stored in this class
+ * Marker(com.google.android.gms.maps.model.Marker) - Reference to the icon that's placed on the google map view, stored in this fragment
  */
 class MapsFragment : Fragment(R.layout.fragment_maps) {
 
@@ -65,39 +65,43 @@ class MapsFragment : Fragment(R.layout.fragment_maps) {
 
         // When the user clicks on the info window open the details page
         googleMap.setOnInfoWindowClickListener {
-            Toast.makeText(context, "CLICKED INFO WINDOW", Toast.LENGTH_SHORT).show()
             val action = MapsFragmentDirections.actionMarkerDetails(3)
             findNavController().navigate(action)
         }
 
+        observeMarkers(googleMap)
+        observeFilterStates(googleMap)
+    }
+
+    private fun observeMarkers(googleMap: GoogleMap) {
         fun createMarkerObserver(markerList: MutableList<Marker>, filterStateIsChecked: LiveData<Boolean>): Observer<List<MapMarker>> =
-                Observer<List<MapMarker>> { markers ->
-                    if (filterStateIsChecked.value != true) return@Observer
+            Observer<List<MapMarker>> { markers ->
+                if (filterStateIsChecked.value != true) return@Observer
 
-                    // Refresh the marker list with the updated list
-                    markerList.removeMarkersFromMapAndClearList()
-                    for (marker in markers)
-                        markerList += googleMap.addMarker( marker.getMarkerOptions() )
-                }
+                // Refresh the marker list with the updated list
+                markerList.removeMarkersFromMapAndClearList()
+                for (marker in markers)
+                    markerList += googleMap.addMarker( marker.getMarkerOptions() )
+            }
 
-        fun <T: MapMarker> createFilterStateObserver(markerList: MutableList<Marker>, mapMarkers: LiveData<List<T>>): Observer<Boolean> =
-                Observer { isChecked ->
-                    if (isChecked)  // Add markers to map
-                        for (marker in mapMarkers.value ?: return@Observer)
-                            markerList += googleMap.addMarker( marker.getMarkerOptions() )
-                    else
-                        markerList.removeMarkersFromMapAndClearList()
-                }
-
-        // Observe MapMarkers in viewModel
         mapsViewModel.lockMarkers.observe(viewLifecycleOwner, createMarkerObserver(lockMarkers, mapsViewModel.lockFilterState))
         mapsViewModel.bridgeGateMarkers.observe(viewLifecycleOwner, createMarkerObserver(bridgeGateMarkers, mapsViewModel.bridgeGateFilterState))
         mapsViewModel.marinaMarkers.observe(viewLifecycleOwner, createMarkerObserver(marinaMarkers, mapsViewModel.marinaFilterState))
         mapsViewModel.cruiseMarkers.observe(viewLifecycleOwner, createMarkerObserver(cruiseMarkers, mapsViewModel.cruiseFilterState))
         mapsViewModel.launchMarkers.observe(viewLifecycleOwner, createMarkerObserver(launchMarkers, mapsViewModel.launchFilterState))
         mapsViewModel.navInfoMarkers.observe(viewLifecycleOwner, createMarkerObserver(navInfoMarkers, mapsViewModel.navInfoFilterState))
+    }
 
-        // Observe filter states
+    private fun observeFilterStates(googleMap: GoogleMap) {
+        fun <T: MapMarker> createFilterStateObserver(markerList: MutableList<Marker>, mapMarkers: LiveData<List<T>>): Observer<Boolean> =
+            Observer { isChecked ->
+                if (isChecked)  // Add markers to map
+                    for (marker in mapMarkers.value ?: return@Observer)
+                        markerList += googleMap.addMarker( marker.getMarkerOptions() )
+                else
+                    markerList.removeMarkersFromMapAndClearList()
+            }
+
         mapsViewModel.lockFilterState.observe(viewLifecycleOwner, createFilterStateObserver(lockMarkers, mapsViewModel.lockMarkers))
         mapsViewModel.bridgeGateFilterState.observe(viewLifecycleOwner, createFilterStateObserver(bridgeGateMarkers, mapsViewModel.bridgeGateMarkers))
         mapsViewModel.marinaFilterState.observe(viewLifecycleOwner, createFilterStateObserver(marinaMarkers, mapsViewModel.marinaMarkers))
@@ -126,27 +130,6 @@ class MapsFragment : Fragment(R.layout.fragment_maps) {
             if (requestCode == MainActivity.REQUEST_CODE)
                 map.isMyLocationEnabled = true
             //Toast.makeText(this, "Permission request granted", Toast.LENGTH_LONG).show()
-        }
-    }
-
-    companion object {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private const val ARG_SECTION_NUMBER = "section_number"
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        @JvmStatic
-        fun newInstance(sectionNumber: Int): MapsFragment {
-            return MapsFragment().apply {
-                arguments = Bundle().apply {
-                    putInt(ARG_SECTION_NUMBER, sectionNumber)
-                }
-            }
         }
     }
 
