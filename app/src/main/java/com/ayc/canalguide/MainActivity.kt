69119125bef3e00366b79429e2d410c55f09c917
James OLeary <olearyj234@gmail.com>
2020-11-10 16:39:38 -0500

    Removed bottom nav bar

--- app/src/main/java/com/ayc/canalguide/MainActivity.kt
+++ app/src/main/java/com/ayc/canalguide/MainActivity.kt
@@ -7,12 +7,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.navigation.NavController
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
@@ -20,16 +15,9 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     //private val mapsViewModel: MapsViewModel by viewModels()
 
-    private lateinit var navController: NavController
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // Setup nav controller and bottom nav view
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        navController = navHostFragment.navController
-        bottom_nav_view.setupWithNavController(navController)
 
         // Request location permissions
         val permissions = arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION)
