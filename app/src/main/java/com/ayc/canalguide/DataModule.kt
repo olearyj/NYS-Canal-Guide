2634de1fb543b5cb469a2d2e9f09dff92be69d04
James OLeary <olearyj234@gmail.com>
2020-11-15 23:32:31 -0500

    Added canals preferences and added to data module

--- app/src/main/java/com/ayc/canalguide/DataModule.kt
+++ app/src/main/java/com/ayc/canalguide/DataModule.kt
@@ -1,7 +1,10 @@
 package com.ayc.canalguide
 
 import android.content.Context
+import android.content.SharedPreferences
+import androidx.preference.PreferenceManager
 import com.ayc.canalguide.data.AppRoomDatabase
+import com.ayc.canalguide.data.CanalPreferences
 import com.ayc.canalguide.network.CanalsApiService
 import com.ayc.canalguide.network.ConnectivityInterceptor
 import com.ayc.canalguide.network.ConnectivityInterceptorImpl
@@ -30,5 +33,13 @@ object DataModule {
     @Provides
     @Singleton
     fun providesCanalsApiService(connectivityInterceptor: ConnectivityInterceptor) = CanalsApiService.invoke(connectivityInterceptor)
+
+    @Provides
+    @Singleton
+    fun providesSharedPreferences(@ApplicationContext context: Context): SharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
+
+    @Provides
+    @Singleton
+    fun providesCanalPreferences(sharedPrefs: SharedPreferences, @ApplicationContext context: Context): CanalPreferences = CanalPreferences(sharedPrefs, context)
 
 }
---
+++ app/src/main/java/com/ayc/canalguide/data/CanalPreferences.kt
@@ -0,0 +1,69 @@
+package com.ayc.canalguide.data
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.core.content.edit
+import com.ayc.canalguide.R
+
+class CanalPreferences(
+    private val prefs: SharedPreferences,
+    context: Context
+) {
+
+
+    // Preference keys
+    private val defaultMapTypeIndexKey = context.getString(R.string.pref_key_default_map_type)
+    private val defaultCacheFiltersKey = context.getString(R.string.pref_key_cache_filters)
+    private val fullscreenModeIndexKey = context.getString(R.string.pref_key_fullscreen_mode)
+
+    private val lockFilterStateKey = context.getString(R.string.pref_key_filter_state_lock)
+    private val bridgeFilterStateKey = context.getString(R.string.pref_key_filter_state_bridge)
+    private val launchFilterStateKey = context.getString(R.string.pref_key_filter_state_launch)
+    private val marinaFilterStateKey = context.getString(R.string.pref_key_filter_state_marina)
+    private val cruiseFilterStateKey = context.getString(R.string.pref_key_filter_state_cruise)
+    private val navinfoFilterStateKey = context.getString(R.string.pref_key_filter_state_navinfo)
+
+
+    // Preferences set in settings fragment
+    val defaultMapTypeIndex: String
+        get() = prefs.getString(defaultMapTypeIndexKey, "0")!!
+
+    val fullscreenMapModeIndex: String
+        get() = prefs.getString(fullscreenModeIndexKey, "0")!!
+
+    private val cacheFilters: Boolean
+        get() = prefs.getBoolean(defaultCacheFiltersKey, false)
+
+
+    // Cached filter states
+    val cachedLockFilterState: Boolean?
+        get() = if (cacheFilters) prefs.getBoolean(lockFilterStateKey, true) else null
+        //private set(value) = prefs.edit().putBoolean(lockFilterStateKey, value).apply()
+
+    val cachedBridgeFilterState: Boolean?
+        get() = if (cacheFilters) prefs.getBoolean(bridgeFilterStateKey, true) else null
+
+    val cachedLaunchFilterState: Boolean?
+        get() = if (cacheFilters) prefs.getBoolean(launchFilterStateKey, true) else null
+
+    val cachedMarinaFilterState: Boolean?
+        get() = if (cacheFilters) prefs.getBoolean(marinaFilterStateKey, true) else null
+
+    val cachedCruiseFilterState: Boolean?
+        get() = if (cacheFilters) prefs.getBoolean(cruiseFilterStateKey, true) else null
+
+    val cachedNavinfoFilterState: Boolean?
+        get() = if (cacheFilters) prefs.getBoolean(navinfoFilterStateKey, false) else null
+
+
+    fun updateCachedFilterStates(lock: Boolean, bridge: Boolean, launch: Boolean, marina: Boolean, cruise: Boolean, navinfo: Boolean) =
+        prefs.edit(commit = true) {
+            putBoolean(lockFilterStateKey, lock)
+            putBoolean(bridgeFilterStateKey, bridge)
+            putBoolean(launchFilterStateKey, launch)
+            putBoolean(marinaFilterStateKey, marina)
+            putBoolean(cruiseFilterStateKey, cruise)
+            putBoolean(navinfoFilterStateKey, navinfo)
+        }
+
+}
